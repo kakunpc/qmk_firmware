45cc34fef0eb1718599e455163652dc775dd935b
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-07-01 23:18:53 +0900

    update keymap

--- keyboards/suihankey/keymaps/kakunpc/keymap.c
+++ keyboards/suihankey/keymaps/kakunpc/keymap.c
@@ -34,19 +34,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1, KC_2, KC_3, KC_4, KC_5,               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_6, KC_7, KC_8, KC_9, KC_0,               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_MINS, KC_EQL, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                  KC_NO, KC_NO, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO),
+              RGB_HUI, RGB_HUD, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO),
 
 	[SKILL_2] = LAYOUT(
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_F11, KC_F12, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                        KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO),
+                    RGB_SAI, KC_NO, RGB_SAD,        KC_NO, KC_NO, KC_NO),
 
 	[SKILL_3] = LAYOUT(
         LCTL(KC_1), LCTL(KC_2), LCTL(KC_3), LCTL(KC_4), LCTL(KC_5),                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         LCTL(KC_6), LCTL(KC_7), LCTL(KC_8), LCTL(KC_9), LCTL(KC_0),                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        LCTL(KC_MINS), LCTL(KC_EQL), KC_NO, KC_NO, KC_NO,                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                                KC_NO, KC_NO, KC_NO,               KC_NO, KC_NO, KC_NO)
+        LCTL(KC_MINS), LCTL(KC_EQL), KC_NO, RGB_MOD, RGB_RMOD,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                            KC_NO, RGB_VAI, RGB_VAD,               KC_NO, KC_NO, KC_NO)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
