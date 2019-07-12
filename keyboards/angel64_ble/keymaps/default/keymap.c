ecab5269a74c5e1689695abede539a6e8d97f15b
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-07-13 02:00:22 +0900

    add keymap

--- keyboards/angel64_ble/keymaps/default/keymap.c
+++ keyboards/angel64_ble/keymaps/default/keymap.c
@@ -43,26 +43,43 @@ enum custom_keycodes {
 
 extern keymap_config_t keymap_config;
 
-enum {
-  BASE=0,
-  META,
-  SYMB,
-  CTRL,
-  ABS_MOUSE,
-  GAME
-};
-
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
-const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
-    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC ,
-        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_ESC,
-    KC_CAPS, KC_LALT, KC_LGUI, KC_LCTL,     KC_SPC,    KC_RCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_RSFT
+enum layers{
+    BASE = 0,
+    COMMAND,
+    SETTING
+};
+
+//#define KC_COMMAND    LT(COMMAND,KC_MHEN)
+//#define KC_SETTING    LT(SETTING,KC_HENK)
+
+#define KC_COMMAND    LT(COMMAND, KC_SPC)
+#define KC_SETTING    LT(SETTING, KC_SPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = LAYOUT(\
+    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC , \
+         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, \
+    KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, \
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_RSFT, \
+    KC_LCTL, KC_LALT, KC_LGUI, KC_COMMAND, KC_SPC,    KC_SETTING, KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL \
+  ),
+  [COMMAND] = LAYOUT(\
+    KC_ESC, KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC , \
+         KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, BATT_LV, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, _______, KC_NO,    _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
+  ),
+  [SETTING] = LAYOUT(
+    AD_WO_L, ADV_ID0,  ADV_ID1,  ADV_ID2, ADV_ID3, ADV_ID4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DELBNDS , \
+         KC_NO, DEL_ID0, DEL_ID1, DEL_ID2, DEL_ID3, DEL_ID4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, _______, BATT_LV,    _______,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
   ),
 };
 
@@ -144,5 +161,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void matrix_init_user(void){
   set_usb_enabled(true);
 }
-
 */
