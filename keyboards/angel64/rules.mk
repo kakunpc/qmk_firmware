6370ab8f6e7e03a5fd3a29b4b329438c6eea5784
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-08-18 14:53:49 +0900

    add led

--- keyboards/angel64/rules.mk
+++ keyboards/angel64/rules.mk
@@ -79,5 +79,6 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 CUSTOM_MATRIX = yes
+OLED_DRIVER_ENABLE = yes
 
 SRC += matrix.c
