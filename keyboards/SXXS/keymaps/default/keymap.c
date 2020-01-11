8cb9cba1797daafa4d4bfd52ca1dd2a7937715f4
kakunpc <15257475+kakunpc@users.noreply.github.com>
2020-01-11 19:52:21 +0900

    set leds

--- keyboards/SXXS/keymaps/default/keymap.c
+++ keyboards/SXXS/keymaps/default/keymap.c
@@ -15,6 +15,25 @@
  */
 #include QMK_KEYBOARD_H
 
+
+int ledIds[] = {1,2,3,4,5,6,0};
+
+int ledcolors[][3] = {
+    {   0,   0, 255 },  // Start
+    {   0,   0, 255 },  // BT-A
+    {   0,   0, 255 },  // BT-B
+    {   0,   0, 255 },  // BT-C
+    {   0,   0, 255 },  // BT-D
+    { 255,   0,   0 },  // FX-L
+    { 255,   0,   0 },  // FX-R
+};
+
+int l_color[] = {   0,   0, 255 };
+int r_color[] = { 255,   0,   0 };
+
+static short l_time = 0;
+static short r_time = 0;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT(
@@ -25,13 +44,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Rotary encoder settings */
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) { /* First encoder */
+  if (index == 0) {
+      if(l_time == -1){
+        setrgb(0,0,0 , (LED_TYPE *)&led[8]);
+        rgblight_set();
+        l_time = 50;
+      }
     if (clockwise) {
       tap_code(KC_S);
     } else {
       tap_code(KC_A);
     }
-  } else if (index == 1) { /* Second encoder */
+  } else if (index == 1) {
+      if(r_time == -1){
+        setrgb(0,0,0 , (LED_TYPE *)&led[7]);
+        rgblight_set();
+        r_time = 50;
+      }
     if (clockwise) {
       tap_code(KC_L);
     } else {
@@ -40,36 +69,52 @@ void encoder_update_user(uint8_t index, bool clockwise) {
   }
 }
 
-int ledIds[] = {1,2,3,4,5,6,0};
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     int col = record->event.key.col;
     int row = record->event.key.row;
     int index =  ledIds[col + (row * 4)];
     if (record->event.pressed) {
-        sethsv(HSV_WHITE, (LED_TYPE *)&led[index]);
+        int r = ledcolors[index][0];
+        int g = ledcolors[index][1];
+        int b = ledcolors[index][2];
+
+        setrgb( r, g, b, (LED_TYPE *)&led[index]);
     }
     else{
         setrgb(0,0,0 , (LED_TYPE *)&led[index]);
     }
-    rgblight_set();
-
+    rgblight_set();;
     return true;
 }
 
+void matrix_scan_user(void) {
+    if(r_time > 0){
+        --r_time;
+    }
+    if( r_time == 0 ){
+        setrgb(r_color[0], r_color[1], r_color[2], (LED_TYPE *)&led[7]);
+        rgblight_set();
+        r_time = -1;
+        return;
+    }
+
+    if(l_time > 0){
+        --l_time;
+    }
+    if( l_time == 0 ){
+        setrgb(l_color[0], l_color[1], l_color[2], (LED_TYPE *)&led[8]);
+        rgblight_set();
+        l_time = -1;
+        return;
+    }
+}
 
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  // debug_enable=true;
-  // Call the post init code.
-  rgblight_enable_noeeprom(); // enables Rgb, without saving settings
-  rgblight_sethsv_noeeprom(180, 255, 255); // sets the color to teal/cyan without saving
-  for(int i = 0; i < 7;++i){
-    int id = ledIds[i];
-    setrgb(0,0,0 , (LED_TYPE *)&led[id]);
+  rgblight_enable_noeeprom();
+  for(int i = 0; i < 9;++i){
+    setrgb(0,0,0 , (LED_TYPE *)&led[i]);
   }
-  // setrgb(150,150,150, (LED_TYPE *)&led[7]);
-  // setrgb(150,150,150, (LED_TYPE *)&led[8]);
+  setrgb(r_color[0], r_color[1], r_color[2], (LED_TYPE *)&led[7]);
+  setrgb(l_color[0], l_color[1], l_color[2], (LED_TYPE *)&led[8]);
   rgblight_set();
-  //rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3); // sets mode to Fast breathing without saving
 }
