84cef41b1604940d3c717b0e78d2d1fdf474e1a6
kakunpc <15257475+kakunpc@users.noreply.github.com>
2020-02-10 17:01:08 +0900

    update

--- keyboards/SXXS/config.h
+++ keyboards/SXXS/config.h
@@ -68,7 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
    #define RGBLIGHT_HUE_STEP 8
    #define RGBLIGHT_SAT_STEP 8
    #define RGBLIGHT_VAL_STEP 8
-   #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
+   #define RGBLIGHT_LIMIT_VAL 80 /* The maximum brightness level */
 //   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 // /*== all animations enable ==*/
 //   #define RGBLIGHT_ANIMATIONS
@@ -93,7 +93,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
-#define TAP_CODE_DELAY 50
+// #define TAP_CODE_DELAY 50
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
--- keyboards/SXXS/keymaps/default/keymap.c
+++ keyboards/SXXS/keymaps/default/keymap.c
@@ -14,107 +14,118 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "rgblight.h"
+#include "rgblight_list.h"
 
+#define ENCODER_WAIT 255
 
-int ledIds[] = {1,2,3,4,5,6,0};
+uint8_t ledIds[] = {1, 2, 3, 4, 5, 6, 0};
 
 int ledcolors[][3] = {
-    {   0,   0, 255 },  // Start
-    {   0,   0, 255 },  // BT-A
-    {   0,   0, 255 },  // BT-B
-    {   0,   0, 255 },  // BT-C
-    {   0,   0, 255 },  // BT-D
-    { 255,   0,   0 },  // FX-L
-    { 255,   0,   0 },  // FX-R
+    {HSV_BLUE},  // Start
+    {HSV_BLUE},  // BT-B
+    {HSV_BLUE},  // BT-C
+    {HSV_BLUE},  // BT-D
+    {HSV_BLUE},  // BT-A
+    {HSV_RED},   // FX-L
+    {HSV_RED},   // FX-R
 };
 
-int l_color[] = {   0,   0, 255 };
-int r_color[] = { 255,   0,   0 };
+int l_color[] = {HSV_BLUE};
+int r_color[] = {HSV_RED};
 
-static short l_time = 0;
-static short r_time = 0;
+static long l_time = 0;
+static uint16_t l_keycode = KC_NO;
+static long r_time = 0;
+static uint16_t r_keycode = KC_NO;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT(
-        KC_D,    KC_F,  KC_J, KC_K,
-        KC_C,    KC_M,  KC_ENT
-    )
-};
+        KC_D, KC_F, KC_J, KC_K,
+            KC_C,       KC_M,
+                 KC_ENT
+                )};
 
 /* Rotary encoder settings */
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (index == 0) {
-      if(l_time == -1){
-        setrgb(0,0,0 , (LED_TYPE *)&led[8]);
-        rgblight_set();
-        l_time = 50;
-      }
-    if (clockwise) {
-      tap_code(KC_S);
-    } else {
-      tap_code(KC_A);
+    if (index == 0) {
+        rgblight_sethsv_at(0, 0, 0, 8);
+        l_time = ENCODER_WAIT;
+        uint16_t new_code;
+        if (clockwise) {
+            new_code = KC_S;
+        } else {
+            new_code = KC_A;
+        }
+        if( new_code != l_keycode){
+            unregister_code(l_keycode);
+            l_keycode = new_code;
+            register_code(l_keycode);
+        }
+    } else if (index == 1) {
+            rgblight_sethsv_at(0, 0, 0, 7);
+            r_time = ENCODER_WAIT;
+        uint16_t new_code;
+        if (clockwise) {
+            new_code = KC_L;
+        } else {
+            new_code = KC_SCLN;
+        }
+        if( new_code != r_keycode){
+            unregister_code(r_keycode);
+            r_keycode = new_code;
+            register_code(r_keycode);
+        }
     }
-  } else if (index == 1) {
-      if(r_time == -1){
-        setrgb(0,0,0 , (LED_TYPE *)&led[7]);
-        rgblight_set();
-        r_time = 50;
-      }
-    if (clockwise) {
-      tap_code(KC_L);
-    } else {
-      tap_code(KC_SCLN);
-    }
-  }
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    int col = record->event.key.col;
-    int row = record->event.key.row;
-    int index =  ledIds[col + (row * 4)];
+    int col   = record->event.key.col;
+    int row   = record->event.key.row;
+    int index = ledIds[col + (row * 4)];
     if (record->event.pressed) {
-        int r = ledcolors[index][0];
-        int g = ledcolors[index][1];
-        int b = ledcolors[index][2];
-
-        setrgb( r, g, b, (LED_TYPE *)&led[index]);
+        int h = ledcolors[index][0];
+        int s = ledcolors[index][1];
+        int v = ledcolors[index][2];
+        rgblight_sethsv_at(h, s, v, index);
+    } else {
+        rgblight_sethsv_at(0, 0, 0, index);
     }
-    else{
-        setrgb(0,0,0 , (LED_TYPE *)&led[index]);
-    }
-    rgblight_set();;
     return true;
 }
 
 void matrix_scan_user(void) {
-    if(r_time > 0){
+    if (r_time > 0) {
         --r_time;
     }
-    if( r_time == 0 ){
-        setrgb(r_color[0], r_color[1], r_color[2], (LED_TYPE *)&led[7]);
-        rgblight_set();
+    if (r_time == 0) {
+        rgblight_sethsv_at(r_color[0], r_color[1], r_color[2], 7);
         r_time = -1;
-        return;
+        if(r_keycode != KC_NO ){
+            unregister_code(r_keycode);
+            r_keycode = KC_NO;
+        }
     }
 
-    if(l_time > 0){
+    if (l_time > 0) {
         --l_time;
     }
-    if( l_time == 0 ){
-        setrgb(l_color[0], l_color[1], l_color[2], (LED_TYPE *)&led[8]);
-        rgblight_set();
+    if (l_time == 0) {
+        rgblight_sethsv_at(l_color[0], l_color[1], l_color[2], 8);
         l_time = -1;
-        return;
+        if(l_keycode != KC_NO ){
+            unregister_code(l_keycode);
+            l_keycode = KC_NO;
+        }
     }
 }
 
 void keyboard_post_init_user(void) {
-  rgblight_enable_noeeprom();
-  for(int i = 0; i < 9;++i){
-    setrgb(0,0,0 , (LED_TYPE *)&led[i]);
-  }
-  setrgb(r_color[0], r_color[1], r_color[2], (LED_TYPE *)&led[7]);
-  setrgb(l_color[0], l_color[1], l_color[2], (LED_TYPE *)&led[8]);
-  rgblight_set();
+    rgblight_enable_noeeprom();
+    for (int i = 0; i < 9; ++i) {
+        rgblight_sethsv_at(0, 0, 0, i);
+    }
+    rgblight_sethsv_at(r_color[0], r_color[1], r_color[2], 7);
+    rgblight_sethsv_at(l_color[0], l_color[1], l_color[2], 8);
 }
