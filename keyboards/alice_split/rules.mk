626c7fd36569ef061d9c764f62f7996b5b92d162
kakunpc <15257475+kakunpc@users.noreply.github.com>
2020-05-02 01:06:39 +0900

    fix default keymap

--- keyboards/alice_split/rules.mk
+++ keyboards/alice_split/rules.mk
@@ -31,4 +31,4 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 SPLIT_KEYBOARD = yes
-OLED_DRIVER_ENABLE = yes
+OLED_DRIVER_ENABLE = no
