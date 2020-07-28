79c33b290d8f7c33dd9925c2c95d6309958dbde1
kakunpc <15257475+kakunpc@users.noreply.github.com>
2020-07-28 22:20:28 +0900

    fix keymap

--- keyboards/alice_split/config.h
+++ keyboards/alice_split/config.h
@@ -252,3 +252,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+
+#define MASTER_RIGHT
